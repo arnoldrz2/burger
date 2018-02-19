19e0aca040264bb22264cd055feeede74e19d084
Arnold R Zimmerman Jr <arnoldrz2@gmail.com>
2018-02-18 20:45:17 -0600

    added burger,js

---
+++ models/burger.js
@@ -0,0 +1,28 @@
+var connection = require ('../config/connection.js');
+var connection = require ('../config/orm.js');
+
+//Create Burger Object
+var burger = {
+    //Select all burger table entries
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+
+    // The variable cols and cals are arrays
+    insertOne: function(col, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+//Export the database functions
+module.exports = burger;
