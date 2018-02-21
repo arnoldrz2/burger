55dc2dbf64d2d0deffd037ad9bf739b3d6ac8007
Arnold R Zimmerman Jr <arnoldrz2@gmail.com>
2018-02-20 23:36:03 -0600

    fix

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,4 @@
-import { createConnection } from "net";
-
 var mysql = require("mysql");
-
 var connection;
 
 if (process.env.JAWSDB_URL) {
@@ -16,13 +13,7 @@ if (process.env.JAWSDB_URL) {
 };
 
 // Connect to the database
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
- });
+connection.connect();
   
  // Export connection
 module.exports = connection;
